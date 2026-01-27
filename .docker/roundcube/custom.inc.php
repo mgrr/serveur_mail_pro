d25345c86a38d5fc2e66bb2fde0c0bda9eacc644
mathieu grenier <webbiexpert@gmail.com>
2026-01-27 05:33:00 +0100

    init

---
+++ .docker/roundcube/custom.inc.php
@@ -0,0 +1,18 @@
+<?php
+$config['imap_conn_options'] = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ),
+);
+$config['smtp_conn_options'] = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ),
+);
+$config['smtp_user'] = '%u';
+$config['smtp_pass'] = '%p';
+$config['plugins'] = array_merge($config['plugins'], ['twofactor_gauthenticator']);
